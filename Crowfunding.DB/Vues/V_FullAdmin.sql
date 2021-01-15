edebb952b50b32f44edb7685c133eb6d144575dd
Legrain Samuel <samuel.legrain@bstorm.be>
2021-01-15 10:57:10 +0100

    Correctif Vues

---
+++ Crowfunding.DB/Vues/V_FullAdmin.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[V_FullAdmin]
+	AS
+SELECT v.ID,
+	v.[FirstName],
+	v.LastName,
+	v.BirthDate,
+	v.[Login],
+	v.Email,
+	v.Phone,
+	v.IDAddress,
+	v.Number,
+	v.Street,
+	v.City,
+	v.Department,
+	v.Country,
+	a.PromotedDate
+FROM [Admin] AS a JOIN
+	[V_FullBasicUser] AS v ON a.IDUser = v.ID
---
+++ Crowfunding.DB/Vues/V_FullProjectCreator.sql
@@ -0,0 +1,26 @@
+﻿CREATE VIEW [dbo].[V_FullProjectCreator]
+	AS
+SELECT v.ID,
+	v.[FirstName],
+	v.LastName,
+	v.BirthDate,
+	v.[Login],
+	v.Email,
+	v.Phone,
+	v.IDAddress,
+	v.Number,
+	v.Street,
+	v.City,
+	v.Department,
+	v.Country,
+	pc.SocietyName,
+	pc.TVANumber,
+	a.IDAddress AS Society_IDAddress,
+	a.Number AS Society_Number,
+	a.Street AS Society_Street,
+	a.City AS Society_City,
+	a.Department AS Society_Departement,
+	a.Country AS Society_Country
+FROM Project_Creator AS pc JOIN
+	V_FullBasicUser AS v ON pc.IDUser = v.ID JOIN
+	[Address] AS a ON pc.IDAddress = a.IDAddress
