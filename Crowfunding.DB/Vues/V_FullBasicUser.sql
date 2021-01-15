cfd91a41038665a251c21bcfdd1dad96d570b0a9
Legrain Samuel <samuel.legrain@bstorm.be>
2021-01-15 09:37:41 +0100

    Add V_FullBasicUser

---
+++ Crowfunding.DB/Vues/V_FullBasicUser.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[V_FullBasicUser]
+	AS 
+SELECT 
+	u.IDUser AS ID,
+	u.FirstName,
+	u.LastName,
+	u.BirthDate,
+	u.[Login],
+	u.Email,
+	u.Phone,
+	a.IDAddress,
+	a.Number,
+	a.Street,
+	a.City,
+	a.Department,
+	a.Country
+FROM [User] AS u JOIN
+	[Address] AS a ON u.IDAddress = a.IDAddress
