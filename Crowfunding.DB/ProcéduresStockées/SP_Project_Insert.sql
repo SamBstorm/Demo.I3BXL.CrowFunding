9be804395c508fb705974fdc9d32dcd620d2a319
Legrain Samuel <samuel.legrain@bstorm.be>
2021-01-21 10:19:51 +0100

    Add Stored Procedure "SP_Project_Insert"

---
+++ Crowfunding.DB/ProcéduresStockées/SP_Project_Insert.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[SP_Project_Insert]
+	@name VARCHAR(64),
+	@desc VARCHAR(MAX),
+	@url VARCHAR(512) = NULL,
+	@fundCap MONEY,
+	@acNb VARCHAR(32),
+	@startDate DATETIME2,
+	@endDate DATETIME2,
+	@idCreator INTEGER
+AS
+	INSERT INTO [Project]([Name],[Description],[URLVideo],[FundingCap],[AccountNumber],[StartDate],[EndDate],[IDCreator])
+		OUTPUT inserted.IDProject
+		VALUES(@name,@desc,@url,@fundCap,@acNb,@startDate,@endDate,@idCreator)
